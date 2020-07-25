529e8d213ecccebe91214616c61b921260a6f8a5
Jonathan Gamboa <jgamboa0209@gmail.com>
2020-07-25 14:36:44 -0400

    Construct Point with default & initial values

--- include/primitives/Point.hpp
+++ include/primitives/Point.hpp
@@ -1,3 +1,13 @@
 #ifndef POINT_HPP
 #define POINT_HPP
+
+struct Point {
+  Point() : x{0}, y{0}, z{0} {}
+  Point(double x, double y, double z) : x{x}, y{y}, z{z} {};
+
+  const double x;
+  const double y;
+  const double z;
+};
+
 #endif
--- src/primitives/Point.test.cpp
+++ src/primitives/Point.test.cpp
@@ -1,2 +1,27 @@
 #include <gtest/gtest.h>
 #include <primitives/Point.hpp>
+
+TEST(Point, defaultConstruct) { Point p; }
+
+TEST(Point, accessThreeSpatialComponents) {
+  Point p;
+  p.x;
+  p.y;
+  p.z;
+}
+
+TEST(Point, componentsDefaultedToZero) {
+  Point p;
+  ASSERT_FLOAT_EQ(p.x, 0.0);
+  ASSERT_FLOAT_EQ(p.y, 0.0);
+  ASSERT_FLOAT_EQ(p.z, 0.0);
+}
+
+TEST(Point, initialValueConstructed) { Point p(1.0, 2.0, 3.0); }
+
+TEST(Point, componentsMatchInitializationInputs) {
+  Point p(1.0, 2.0, 3.0);
+  ASSERT_FLOAT_EQ(p.x, 1.0);
+  ASSERT_FLOAT_EQ(p.y, 2.0);
+  ASSERT_FLOAT_EQ(p.z, 3.0);
+}
