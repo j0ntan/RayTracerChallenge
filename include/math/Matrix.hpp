bde6025ddcb0020cd5db8306df612f417142ebfd
Jonathan Gamboa <jgamboa0209@gmail.com>
2020-08-04 11:25:46 -0400

    Construct Matrix with initializer list

--- include/math/Matrix.hpp
+++ include/math/Matrix.hpp
@@ -3,9 +3,39 @@
 
 #include <algorithm>
 #include <array>
+#include <utility>
 
 template <std::size_t n = 2> struct Matrix {
   Matrix() { std::fill(values.begin(), values.end(), 0.); }
+  Matrix(std::initializer_list<double> simple_list) {
+    const auto initialization_count =
+        n * n <= simple_list.size() ? n * n : simple_list.size();
+    std::copy(simple_list.begin(), simple_list.begin() + initialization_count,
+              values.begin());
+
+    if (initialization_count < n * n)
+      std::fill_n(values.begin() + initialization_count,
+                  n * n - initialization_count, 0.);
+  }
+  Matrix(std::initializer_list<std::initializer_list<double>> nested_list) {
+    auto inner_list_it = nested_list.begin();
+    const double *value_it;
+    std::size_t index = 0;
+
+    std::size_t list_count = n <= nested_list.size() ? n : nested_list.size();
+    for (std::size_t row = 0; row < list_count; ++row, ++inner_list_it) {
+      value_it = inner_list_it->begin();
+      for (std::size_t column = 0; column < n; ++column, ++value_it, ++index) {
+        if (column < inner_list_it->size())
+          values[index] = *value_it;
+        else
+          values[index] = 0;
+      }
+    }
+
+    if (index < n * n)
+      std::fill_n(values.begin() + index, (n * n - index), 0);
+  }
 
   double &operator()(std::size_t row, std::size_t column) {
     return values[row * n + column];
--- src/math/Matrix.test.cpp
+++ src/math/Matrix.test.cpp
@@ -26,3 +26,75 @@ TEST(Matrix, allElementsDefaultToZero) {
     for (std::size_t col; col < 3; ++col)
       ASSERT_FLOAT_EQ(m(row, col), 0);
 }
+
+TEST(Matrix, constructWithSimpleList) {
+  Matrix<2> m = {1, 2, 3, 4};
+  ASSERT_FLOAT_EQ(m(0, 0), 1);
+  ASSERT_FLOAT_EQ(m(0, 1), 2);
+  ASSERT_FLOAT_EQ(m(1, 0), 3);
+  ASSERT_FLOAT_EQ(m(1, 1), 4);
+}
+
+TEST(Matrix, useSimpleListWithNonMatchingSize) {
+  Matrix<2> m1 = {1};
+  ASSERT_FLOAT_EQ(m1(0, 0), 1);
+  ASSERT_FLOAT_EQ(m1(0, 1), 0);
+  ASSERT_FLOAT_EQ(m1(1, 0), 0);
+  ASSERT_FLOAT_EQ(m1(1, 1), 0);
+
+  Matrix<2> m2 = {1, 2, 3, 4, 5, 6};
+  ASSERT_FLOAT_EQ(m2(0, 0), 1);
+  ASSERT_FLOAT_EQ(m2(0, 1), 2);
+  ASSERT_FLOAT_EQ(m2(1, 0), 3);
+  ASSERT_FLOAT_EQ(m2(1, 1), 4);
+}
+
+TEST(Matrix, constructWithNestedLists) {
+  Matrix<2> m = {{1, 2}, {3, 4}};
+  ASSERT_FLOAT_EQ(m(0, 0), 1);
+  ASSERT_FLOAT_EQ(m(0, 1), 2);
+  ASSERT_FLOAT_EQ(m(1, 0), 3);
+  ASSERT_FLOAT_EQ(m(1, 1), 4);
+}
+
+TEST(Matrix, useNestedListWithRowsOfNonMatchingSize) {
+  Matrix<2> m1 = {{1}, {2, 3, 4}};
+  ASSERT_FLOAT_EQ(m1(0, 0), 1);
+  ASSERT_FLOAT_EQ(m1(0, 1), 0);
+  ASSERT_FLOAT_EQ(m1(1, 0), 2);
+  ASSERT_FLOAT_EQ(m1(1, 1), 3);
+}
+
+TEST(Matrix, useNestedListWithNonMatchingRows) {
+  Matrix<2> m1 = {{1, 2}};
+  ASSERT_FLOAT_EQ(m1(0, 0), 1);
+  ASSERT_FLOAT_EQ(m1(0, 1), 2);
+  ASSERT_FLOAT_EQ(m1(1, 0), 0);
+  ASSERT_FLOAT_EQ(m1(1, 1), 0);
+
+  Matrix<2> m2 = {{1, 2}, {3, 4}, {5, 6}};
+  ASSERT_FLOAT_EQ(m2(0, 0), 1);
+  ASSERT_FLOAT_EQ(m2(0, 1), 2);
+  ASSERT_FLOAT_EQ(m2(1, 0), 3);
+  ASSERT_FLOAT_EQ(m2(1, 1), 4);
+}
+
+TEST(Matrix, useNestedListWithMixedSize) {
+  Matrix<4> m = {{1, 2, 3, 4, 5}, {6, 7}, {8, 9, 10, 11}};
+  ASSERT_FLOAT_EQ(m(0, 0), 1);
+  ASSERT_FLOAT_EQ(m(0, 1), 2);
+  ASSERT_FLOAT_EQ(m(0, 2), 3);
+  ASSERT_FLOAT_EQ(m(0, 3), 4);
+  ASSERT_FLOAT_EQ(m(1, 0), 6);
+  ASSERT_FLOAT_EQ(m(1, 1), 7);
+  ASSERT_FLOAT_EQ(m(1, 2), 0);
+  ASSERT_FLOAT_EQ(m(1, 3), 0);
+  ASSERT_FLOAT_EQ(m(2, 0), 8);
+  ASSERT_FLOAT_EQ(m(2, 1), 9);
+  ASSERT_FLOAT_EQ(m(2, 2), 10);
+  ASSERT_FLOAT_EQ(m(2, 3), 11);
+  ASSERT_FLOAT_EQ(m(3, 0), 0);
+  ASSERT_FLOAT_EQ(m(3, 1), 0);
+  ASSERT_FLOAT_EQ(m(3, 2), 0);
+  ASSERT_FLOAT_EQ(m(3, 3), 0);
+}
