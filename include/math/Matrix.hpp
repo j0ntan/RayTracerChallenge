be56c46bbbdd5709d3b9426f448aa0ebab37d8ee
Jonathan Gamboa <jgamboa0209@gmail.com>
2020-08-04 07:24:30 -0400

    Add basic Matrix definition

--- include/math/Matrix.hpp
+++ include/math/Matrix.hpp
@@ -1,4 +1,23 @@
 #ifndef MATRIX_HPP
 #define MATRIX_HPP
 
+#include <algorithm>
+#include <array>
+
+template <std::size_t n = 2> struct Matrix {
+  Matrix() { std::fill(values.begin(), values.end(), 0.); }
+
+  double &operator()(std::size_t row, std::size_t column) {
+    return values[row * n + column];
+  }
+  double operator()(std::size_t row, std::size_t column) const {
+    return values[row * n + column];
+  }
+
+  std::array<double, n * n> values;
+  static const std::size_t size;
+};
+
+template <std::size_t n> const std::size_t Matrix<n>::size = n;
+
 #endif
--- src/math/Matrix.test.cpp
+++ src/math/Matrix.test.cpp
@@ -1,2 +1,28 @@
 #include <gtest/gtest.h>
 #include <math/Matrix.hpp>
+
+TEST(Matrix, defaultConstruct) { Matrix<> m; }
+
+TEST(Matrix, defaultSizeIsTwo) { ASSERT_EQ(Matrix<>().size, 2); }
+
+TEST(Matrix, constructWithGivenSize) {
+  Matrix<3> m;
+  ASSERT_EQ(m.size, 3);
+}
+
+TEST(Matrix, accessValues) { Matrix<>().values[0]; }
+
+TEST(Matrix, writeValuesUsingIndexOperator) {
+  Matrix<> m;
+  m(0, 0) = 1;
+  ASSERT_FLOAT_EQ(m(0, 0), 1);
+  m(1, 0) = 2;
+  ASSERT_FLOAT_EQ(m(1, 0), 2);
+}
+
+TEST(Matrix, allElementsDefaultToZero) {
+  Matrix<3> m;
+  for (std::size_t row; row < 3; ++row)
+    for (std::size_t col; col < 3; ++col)
+      ASSERT_FLOAT_EQ(m(row, col), 0);
+}
