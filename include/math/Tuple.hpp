2e7da012f8285670021b256002b088596d1de9b7
Jonathan Gamboa <jgamboa0209@gmail.com>
2020-08-08 05:13:37 -0400

    Add basic Tuple definition

--- include/math/Tuple.hpp
+++ include/math/Tuple.hpp
@@ -1,4 +1,19 @@
 #ifndef TUPLE_HPP
 #define TUPLE_HPP
 
+#include <algorithm>
+#include <array>
+
+template <std::size_t n = 0> struct Tuple {
+  Tuple() = default;
+  Tuple(const std::initializer_list<double> &list) {
+    std::copy(list.begin(), list.begin() + (n <= list.size() ? n : list.size()),
+              elements.begin());
+  }
+  Tuple(const std::array<double, n> &array) { elements = array; }
+
+  std::array<double, n> elements;
+  const std::size_t &size = elements.size();
+};
+
 #endif
--- src/math/Tuple.test.cpp
+++ src/math/Tuple.test.cpp
@@ -1,2 +1,45 @@
 #include <gtest/gtest.h>
 #include <math/Tuple.hpp>
+
+TEST(Tuple, defaultConstruct) { Tuple<> t; }
+
+TEST(Tuple, getSize) { std::size_t size = Tuple<>().size; }
+
+TEST(Tuple, emptyTupleHasSizeZero) { ASSERT_EQ(Tuple<>().size, 0); }
+
+TEST(Tuple, initializeWithNonZeroSize) { Tuple<3> t; }
+
+TEST(Tuple, sizeMatchesInitializationParameter) {
+  ASSERT_EQ(Tuple<3>().size, 3);
+}
+
+TEST(Tuple, accessElements) { Tuple<3>().elements; }
+
+TEST(Tuple, initializeWithList) { Tuple<3> t = {1, 2, 3}; }
+
+TEST(Tuple, matchValuesInInitializationList) {
+  Tuple<3> t = {1, 2, 3};
+  ASSERT_FLOAT_EQ(t.elements[0], 1);
+  ASSERT_FLOAT_EQ(t.elements[1], 2);
+  ASSERT_FLOAT_EQ(t.elements[2], 3);
+}
+
+TEST(Tuple, makeUninitializedElementsDefaultToZero) {
+  const Tuple<3> ct = {1.1};
+  ASSERT_FLOAT_EQ(ct.elements[0], 1.1);
+  ASSERT_FLOAT_EQ(ct.elements[1], 0);
+  ASSERT_FLOAT_EQ(ct.elements[2], 0);
+}
+
+TEST(Tuple, initializeWithArray) {
+  std::array<double, 3> values = {1, 2, 3};
+  Tuple<3> t(values);
+}
+
+TEST(Tuple, matchValuesInInitializationArray) {
+  std::array<double, 3> values = {1, 2, 3};
+  Tuple<3> t(values);
+  ASSERT_FLOAT_EQ(t.elements[0], 1);
+  ASSERT_FLOAT_EQ(t.elements[1], 2);
+  ASSERT_FLOAT_EQ(t.elements[2], 3);
+}
