8c21eca348e20438378c63cc9d9a1458442248b2
Jonathan Gamboa <jgamboa0209@gmail.com>
2021-01-06 08:38:06 -0500

    Move floating-point epsilon to header & add documentation

--- include/utility/Float_compare.hpp
+++ include/utility/Float_compare.hpp
@@ -1,6 +1,11 @@
 #ifndef FLOAT_COMPARE_HPP
 #define FLOAT_COMPARE_HPP
 
+/**
+ * @brief Maximum relative error defined for floating-point numbers.
+ */
+const double EPSILON = 0.0001;
+
 bool float_equals(const double number1, const double number2);
 
 #endif
