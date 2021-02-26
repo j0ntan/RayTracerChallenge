b88e4ebe9d58fae904722847a012e779e6b77dc4
Jonathan Gamboa <jgamboa0209@gmail.com>
2021-02-26 10:29:13 -0500

    Calculate intersection values

--- src/math/Intersections.cpp
+++ src/math/Intersections.cpp
@@ -1,5 +1,22 @@
+#include <cmath>
 #include <math/Intersections.hpp>
 
 std::vector<double> intersect(const Sphere &sphere, const Ray &ray) {
-  return std::vector<double>();
+  std::vector<double> result;
+
+  const auto sphere_to_ray = ray.origin - Point(0, 0, 0);
+
+  const auto a = dot(ray.direction, ray.direction);
+  const auto b = 2 * dot(ray.direction, sphere_to_ray);
+  const auto c = dot(sphere_to_ray, sphere_to_ray) - 1;
+  const auto discriminant = b * b - 4 * a * c;
+
+  if (discriminant >= 0) {
+    const auto t1 = (-b - sqrt(discriminant)) / (2 * a);
+    const auto t2 = (-b + sqrt(discriminant)) / (2 * a);
+    result.push_back(t1);
+    result.push_back(t2);
+  }
+
+  return result;
 }
--- src/math/Intersections.test.cpp
+++ src/math/Intersections.test.cpp
@@ -12,3 +12,46 @@ TEST(Intersections, getVectorOfTimeValues) {
   Sphere s;
   std::vector<double> xs = intersect(s, r);
 }
+
+TEST(Intersections, intersectSphereAtTwoPoints) {
+  Ray r(Point(0, 0, -5), Vector(0, 0, 1));
+  Sphere s;
+  auto xs = intersect(s, r);
+  ASSERT_EQ(xs.size(), 2);
+  ASSERT_FLOAT_EQ(xs[0], 4.0);
+  ASSERT_FLOAT_EQ(xs[1], 6.0);
+}
+
+TEST(Intersections, intersectSphereAtTangent) {
+  Ray r(Point(0, 1, -5), Vector(0, 0, 1));
+  Sphere s;
+  auto xs = intersect(s, r);
+  ASSERT_EQ(xs.size(), 2);
+  ASSERT_FLOAT_EQ(xs[0], 5.0);
+  ASSERT_FLOAT_EQ(xs[1], 5.0);
+}
+
+TEST(Intersections, noIntersectionOnRayMiss) {
+  Ray r(Point(0, 2, -5), Vector(0, 0, 1));
+  Sphere s;
+  auto xs = intersect(s, r);
+  ASSERT_EQ(xs.size(), 0);
+}
+
+TEST(Intersections, rayInsideSphere) {
+  Ray r(Point(0, 0, 0), Vector(0, 0, 1));
+  Sphere s;
+  auto xs = intersect(s, r);
+  ASSERT_EQ(xs.size(), 2);
+  ASSERT_FLOAT_EQ(xs[0], -1.0);
+  ASSERT_FLOAT_EQ(xs[1], 1.0);
+}
+
+TEST(Intersections, rayBehindSphere) {
+  Ray r(Point(0, 0, 5), Vector(0, 0, 1));
+  Sphere s;
+  auto xs = intersect(s, r);
+  ASSERT_EQ(xs.size(), 2);
+  ASSERT_FLOAT_EQ(xs[0], -6.0);
+  ASSERT_FLOAT_EQ(xs[1], -4.0);
+}
