b3f3d9d6097c285772cdb8255c9bd7d542f2e2aa
Jonathan Gamboa <jgamboa0209@gmail.com>
2021-03-08 02:36:12 -0500

    Calculate lighting Color

--- src/scene/Lighting.cpp
+++ src/scene/Lighting.cpp
@@ -1,7 +1,33 @@
+#include <cmath>
 #include <scene/Lighting.hpp>
 
 Color lighting(const Material &surface_mat, const Light &source,
                const Point &illuminated, const Vector &eye,
                const Vector &surface_norm) {
-  return Color();
+  Color ambient, diffuse, specular;
+
+  const auto EFFECTIVE_COLOR = surface_mat.color * source.intensity;
+
+  const auto LIGHT_V = Vector(source.position - illuminated).normalize();
+
+  ambient = EFFECTIVE_COLOR * surface_mat.ambient;
+
+  const auto LIGHT_PROJECTION = dot(LIGHT_V, surface_norm);
+  if (LIGHT_PROJECTION < 0)
+    diffuse = specular = Color(0, 0, 0); // black
+  else {
+    diffuse = EFFECTIVE_COLOR * surface_mat.diffuse * LIGHT_PROJECTION;
+
+    const auto REFLECTION_V = reflect(-LIGHT_V, surface_norm);
+
+    const auto REFLECTION_PROJECTION = dot(REFLECTION_V, eye);
+    if (REFLECTION_PROJECTION <= 0)
+      specular = Color(0, 0, 0);
+    else {
+      const auto FACTOR = pow(REFLECTION_PROJECTION, surface_mat.shininess);
+      specular = source.intensity * surface_mat.specular * FACTOR;
+    }
+  }
+
+  return ambient + diffuse + specular;
 }
--- src/scene/Lighting.test.cpp
+++ src/scene/Lighting.test.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <gtest/gtest.h>
 #include <scene/Lighting.hpp>
 
@@ -16,4 +17,44 @@ TEST_F(Lighting_F, callFunction) {
 TEST_F(Lighting_F, returnsColor) {
   Color color = lighting(Material(), Light(Point(), Color()), Point(), Vector(),
                          Vector());
+}
+
+TEST_F(Lighting_F, eyeBetweenLightAndSurface) {
+  auto eye = Vector(0, 0, -1);
+  auto light_source = Light(Point(0, 0, -10), light_color);
+  auto result = lighting(default_material, light_source, illuminated, eye,
+                         surface_normal);
+  ASSERT_EQ(result, Color(1.9, 1.9, 1.9));
+}
+
+TEST_F(Lighting_F, eyeAt45DegreesBetweenLightAndSurface) {
+  auto eye = Vector(0, sqrt(2) / 2, -sqrt(2) / 2);
+  auto light_source = Light(Point(0, 0, -10), light_color);
+  auto result = lighting(default_material, light_source, illuminated, eye,
+                         surface_normal);
+  ASSERT_EQ(result, Color(1.0, 1.0, 1.0));
+}
+
+TEST_F(Lighting_F, eyeOppositeSurfaceWithLightOffsetBy45Degrees) {
+  auto eye = Vector(0, 0, -1);
+  auto light_source = Light(Point(0, 10, -10), light_color);
+  auto result = lighting(default_material, light_source, illuminated, eye,
+                         surface_normal);
+  ASSERT_EQ(result, Color(0.7364, 0.7364, 0.7364));
+}
+
+TEST_F(Lighting_F, eyeInPathOfReflectionVector) {
+  auto eye = Vector(0, -sqrt(2) / 2, -sqrt(2) / 2);
+  auto light_source = Light(Point(0, 10, -10), light_color);
+  auto result = lighting(default_material, light_source, illuminated, eye,
+                         surface_normal);
+  ASSERT_EQ(result, Color(1.6364, 1.6364, 1.6364));
+}
+
+TEST_F(Lighting_F, lightBehindTheSurface) {
+  auto eye = Vector(0, 0, -1);
+  auto light_source = Light(Point(0, 0, 10), light_color);
+  auto result = lighting(default_material, light_source, illuminated, eye,
+                         surface_normal);
+  ASSERT_EQ(result, Color(0.1, 0.1, 0.1));
 }
