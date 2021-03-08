5a5df3e44a20ffa93aabe325dba347f86bd27f2b
Jonathan Gamboa <jgamboa0209@gmail.com>
2021-03-08 02:07:41 -0500

    Refactor lighting tests; use fixture
    A gtest fixture was used in anticipation of the next set of tests which
    uses a few shared components.

--- src/scene/Lighting.test.cpp
+++ src/scene/Lighting.test.cpp
@@ -1,11 +1,19 @@
 #include <gtest/gtest.h>
 #include <scene/Lighting.hpp>
 
-TEST(Lighting, callFunction) {
+class Lighting_F : public ::testing::Test {
+public:
+  const Material default_material;
+  const Point illuminated;
+  const Color light_color = Color(1, 1, 1);
+  const Vector surface_normal = Vector(0, 0, -1);
+};
+
+TEST_F(Lighting_F, callFunction) {
   lighting(Material(), Light(Point(), Color()), Point(), Vector(), Vector());
 }
 
-TEST(Lighting, returnsColor) {
+TEST_F(Lighting_F, returnsColor) {
   Color color = lighting(Material(), Light(Point(), Color()), Point(), Vector(),
                          Vector());
 }
