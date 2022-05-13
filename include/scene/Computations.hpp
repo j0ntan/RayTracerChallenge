7abbb9992f74878b1128bbd30b36572821050934
Jonathan Gamboa <jgamboa0209@gmail.com>
2022-05-13 17:49:33 -0400

    Add Computations components

--- include/scene/Computations.hpp
+++ include/scene/Computations.hpp
@@ -1,6 +1,16 @@
 #ifndef COMPUTATIONS_HPP
 #define COMPUTATIONS_HPP
 
-struct Computations;
+#include <math/Point.hpp>
+#include <math/Vector.hpp>
+#include <scene/Sphere.hpp>
+
+struct Computations {
+  double t;
+  const Sphere *object;
+  Point point;
+  Vector eyev;
+  Vector normalv;
+};
 
 #endif
--- src/scene/Computations.test.cpp
+++ src/scene/Computations.test.cpp
@@ -1,2 +1,13 @@
 #include <gtest/gtest.h>
 #include <scene/Computations.hpp>
+
+TEST(Computations, createDefault) { Computations computations; }
+
+TEST(Computations, hasComponents) {
+  Computations computations;
+  double time = computations.t;
+  const Sphere *sphere = computations.object;
+  Point point = computations.point;
+  Vector eye = computations.eyev;
+  Vector normal = computations.normalv;
+}
