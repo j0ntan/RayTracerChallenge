34dbf8c21f474be9915740a2abdf32310e35999a
Jonathan Gamboa <jgamboa0209@gmail.com>
2024-08-16 19:29:45 -0400

    Refactor & add documentation to `Point`

--- include/math/Point.hpp
+++ include/math/Point.hpp
@@ -1,16 +1,60 @@
-#ifndef POINT_HPP
-#define POINT_HPP
+#pragma once
 
 #include "Tuple.hpp"
 
+/**
+ * @brief A point in the ray tracer 3D world-space
+ *
+ * A `Point` holds `x`,`y` & `z` components. The fourth component, `w` = 1,
+ * helps to differentiate a Point from a Vector, since they are both 4-element
+ * Tuples.
+ *
+ */
 struct Point final : public Tuple<4> {
+
+  /**
+   * @brief Construct a new Point object at the origin (0,0,0).
+   *
+   */
   Point();
+
+  /**
+   * @brief Construct a new Point object with given initial values
+   *
+   * @param x,y,z Initial values
+   */
   Point(double x, double y, double z);
+
+  /**
+   * @brief Construct a new Point object with a Tuple containing initial values
+   *
+   * @param values Initial values
+   */
   explicit Point(const Tuple<3> &values);
+
+  /**
+   * @brief Construct a new Point object with a Tuple containing initial values
+   *
+   * Note: The fourth value in the Tuple is ignored, since `w` is always 1.
+   *
+   * @param values Initial values
+   */
   explicit Point(const Tuple<4> &values);
 
-  Point &operator=(const Point &rhs);
+  /**
+   * @brief Overload for the assignment operator by default assignment
+   *
+   * @param rhs The `Point` whose values are assigned to the caller
+   * @return Point& Returns the caller after assignment
+   */
+  Point &operator=(const Point &rhs) = default;
 
+  /**
+   * @brief Accessor functions for each `Point` component
+   *
+   * @return double&, double The accessed component returned by reference or by
+   * value, respectively
+   */
   double &x();
   double &y();
   double &z();
@@ -18,5 +62,3 @@ struct Point final : public Tuple<4> {
   double y() const;
   double z() const;
 };
-
-#endif
--- src/math/Point.cpp
+++ src/math/Point.cpp
@@ -1,20 +1,12 @@
 #include <math/Point.hpp>
-#include <utility/Float_compare.hpp>
 
 Point::Point() : Tuple<4>{0, 0, 0, 1} {}
 
-Point::Point(double x, double y, double z) : Tuple<4>{x, y, z, 1} {};
+Point::Point(double x, double y, double z) : Tuple<4>{x, y, z, 1} {}
 
-Point::Point(const Tuple<3> &values)
-    : Tuple<4>{values.elements[0], values.elements[1], values.elements[2], 1} {}
+Point::Point(const Tuple<3> &values) : Point(values[0], values[1], values[2]) {}
 
-Point::Point(const Tuple<4> &values)
-    : Tuple<4>{values.elements[0], values.elements[1], values.elements[2], 1} {}
-
-Point &Point::operator=(const Point &rhs) {
-  elements = rhs.elements;
-  return *this;
-}
+Point::Point(const Tuple<4> &values) : Point(values[0], values[1], values[2]) {}
 
 double &Point::x() { return elements[0]; }
 
