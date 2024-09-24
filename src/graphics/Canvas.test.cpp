5e8966f46b5ae4abb8ea9b450dbdf69a9e7aad5e
Jonathan Gamboa <jgamboa0209@gmail.com>
2024-09-24 00:58:57 -0400

    Refactor `Canvas` tests

--- src/graphics/Canvas.test.cpp
+++ src/graphics/Canvas.test.cpp
@@ -4,38 +4,52 @@
 #include <gtest/gtest.h>
 #include <sstream>
 
-TEST(Canvas, constructBasicCanvas) {
+TEST(CanvasConstructor, constructBasicCanvas) {
   const size_t WIDTH = 5, HEIGHT = 3;
   Canvas c(WIDTH, HEIGHT);
 }
 
-TEST(Canvas, widthAndHeightAreInitialized) {
+TEST(CanvasSize, accessWidthAndHeight) {
+  const size_t WIDTH = 100, HEIGHT = 100;
+  Canvas canvas(WIDTH, HEIGHT);
+  size_t canvas_width = canvas.width;
+  size_t canvas_height = canvas.height;
+}
+
+TEST(CanvasSize, matchInitializedSize) {
   const size_t WIDTH = 100, HEIGHT = 100;
   Canvas canvas(WIDTH, HEIGHT);
   ASSERT_EQ(canvas.width, WIDTH);
   ASSERT_EQ(canvas.height, HEIGHT);
 }
 
-TEST(Canvas, getColorFromPixel) {
+TEST(CanvasPixel, accessPixelColor) {
   Canvas canvas{100, 100};
   Color c = canvas.pixel(0, 0);
 }
 
-TEST(Canvas, allPixelsDefaultToBlack) {
+TEST(CanvasConstructor, matchEmptyCanvasDefaultBlackPixels) {
   Canvas canvas{100, 100};
-  const Color black{0, 0, 0};
+  const Color BLACK{0, 0, 0};
   for (unsigned x = 0; x < canvas.width; ++x)
     for (unsigned y = 0; y < canvas.height; ++y)
-      ASSERT_EQ(canvas.pixel(x, y), black);
+      ASSERT_EQ(canvas.pixel(x, y), BLACK);
 }
 
-TEST(Canvas, writeColorToPixelAtXYCoordinates) {
+TEST(CanvasWrite, writeToCanvas) {
   Canvas canvas{100, 100};
-  const Color red(1, 0, 0), blue(0, 1, 0);
-  canvas.write(2, 3, red);
-  canvas.write(4, 8, blue);
-  ASSERT_EQ(canvas.pixel(2, 3), red);
-  ASSERT_EQ(canvas.pixel(4, 8), blue);
+  const size_t X = 2, Y = 3;
+  const Color RED(1, 0, 0);
+  canvas.write(X, Y, RED);
+}
+
+TEST(CanvasWrite, matchWrittenColors) {
+  Canvas canvas{100, 100};
+  const Color RED(1, 0, 0), BLUE(0, 1, 0);
+  canvas.write(2, 3, RED);
+  canvas.write(4, 8, BLUE);
+  ASSERT_EQ(canvas.pixel(2, 3), RED);
+  ASSERT_EQ(canvas.pixel(4, 8), BLUE);
 }
 
 TEST(PPM, convertToPPM) {
